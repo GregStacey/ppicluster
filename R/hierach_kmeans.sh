f33cb7f350e71ea5fd4c190c7e8cbc3fcb7d7624
Greg <richard.greg.stacey@gmail.com>
2019-03-25 16:24:21 -0700

    Add sbatch

---
+++ R/hierach_kmeans.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -o hierarch_kmeans.out
+#SBATCH -e hierarch_kmeans.err
+#SBATCH --mail-user=richard.greg.stacey@gmail.com
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --time=23:55:00
+#SBATCH --mem=16000M
+
+module load r/3.4.3
+
+Rscript hierarch_kmeans.R
+
