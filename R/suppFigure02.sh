33067150c2126cdebf1c82d3fb68f5c41b35b71d
Greg Stacey <gregstacey@dhcp-128-189-220-55.ubcsecure.wireless.ubc.ca>
2019-05-28 11:51:05 -0700

    Modify supfig02 slurm script

--- R/suppFigure02.sh
+++ R/suppFigure02.sh
@@ -3,7 +3,7 @@
 #SBATCH -e supFig03.err
 #SBATCH --mail-user=richard.greg.stacey@gmail.com
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --time=23:55:00
+#SBATCH --time=2:55:00
 #SBATCH --mem=16000M
 
 module load r/3.4.3
