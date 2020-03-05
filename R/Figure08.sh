2474444283ba6af124f8fae16451e3df3803dfc3
Greg Stacey <gregstacey@dhcp-206-87-114-121.ubcsecure.wireless.ubc.ca>
2020-03-05 13:54:35 -0800

    Change dependencies

--- R/Figure08.sh
+++ R/Figure08.sh
@@ -6,6 +6,9 @@
 #SBATCH --time=23:55:00
 #SBATCH --mem=16000M
 
-module load r/3.4.3
+module load gcc/7.3.0
+module load nixpkgs/16.09
+module load netcdf/4.6.1
+module load r/3.6.0
 
 Rscript Figure08.R
