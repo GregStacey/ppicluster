a5072fe28cca5337f69e1814ae55dd28769f9684
Greg Stacey <gregstacey@Gregs-MacBook-Pro.local>
2020-07-07 17:24:52 -0700

    Increase time request

--- R/cofrac_MCPrevisions.sh
+++ R/cofrac_MCPrevisions.sh
@@ -3,7 +3,7 @@
 #SBATCH -e cofrac_MCPrevisions.err
 #SBATCH --mail-user=richard.greg.stacey@gmail.com
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --time=02:55:00
+#SBATCH --time=23:55:00
 #SBATCH --mem=4G
 #SBATCH --array=1-56
 #SBATCH --account=rrg-ljfoster-ab
