5820b20d7886f0f2f372d5081f8ccf5bd3bd995b
Greg Stacey <gregstacey@Gregs-MacBook-Pro.local>
2020-07-07 17:24:10 -0700

    Add slurm script for cofrac_MCPrevisions

---
+++ R/cofrac_MCPrevisions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -o cofrac_MCPrevisions.out
+#SBATCH -e cofrac_MCPrevisions.err
+#SBATCH --mail-user=richard.greg.stacey@gmail.com
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --time=02:55:00
+#SBATCH --mem=4G
+#SBATCH --array=1-56
+#SBATCH --account=rrg-ljfoster-ab
+
+module load gcc/7.3.0
+module load r/3.6.0
+source ~/ENV3/bin/activate
+
+Rscript cofrac_MCPrevisions.R
