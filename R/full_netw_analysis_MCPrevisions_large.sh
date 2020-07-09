632a2b8365ed0322dc93fa778ddf0d10e3055702
Greg Stacey <gregstacey@Gregs-MacBook-Pro.local>
2020-07-09 14:46:00 -0700

    Reduce time request

--- R/full_netw_analysis_MCPrevisions_large.sh
+++ R/full_netw_analysis_MCPrevisions_large.sh
@@ -3,7 +3,7 @@
 #SBATCH -e full_netw_analysis_MCPrevisions_large.err
 #SBATCH --mail-user=richard.greg.stacey@gmail.com
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --time=71:55:00
+#SBATCH --time=23:55:00
 #SBATCH --mem=64G
 #SBATCH --array=1-144
 #SBATCH --account=rrg-ljfoster-ab
