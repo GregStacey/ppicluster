723a7b507dc0b15c8865d912edb9d4396ae5e3a8
Greg Stacey <gregstacey@Gregs-MacBook-Pro.local>
2020-07-16 15:36:05 -0700

    Change output to /scratch folder

--- scheduler/test.sh
+++ scheduler/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -N full_netw_analysis_MCPrevisions_large
 #PBS -l walltime=00:01:00
-#PBS -e /home/staceyri/projects/ppicluster/scheduler/full_netw_analysis_MCPrevisions_large.err
-#PBS -o /home/staceyri/projects/ppicluster/scheduler/full_netw_analysis_MCPrevisions_large.out
+#PBS -e /scratch/st-ljfoster-1/ppicluster/scheduler/full_netw_analysis_MCPrevisions_large.err
+#PBS -o /scratch/st-ljfoster-1/ppicluster/scheduler/full_netw_analysis_MCPrevisions_large.out
 #PBS -j oe
 #PBS -k eod
 #PBS -m abe
