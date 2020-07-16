38c77909adfa558fb4af6de16955ea45f16e9f71
Greg Stacey <gregstacey@Gregs-MacBook-Pro.local>
2020-07-16 16:08:22 -0700

    First attempt at correct sockeye pbs submission script

--- scheduler/full_netw_analysis_MCPrevisions.sh
+++ scheduler/full_netw_analysis_MCPrevisions.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 #PBS -N full_netw_analysis_MCPrevisions_large
 #PBS -l walltime=71:55:00
-#PBS -j oe
-#PBS -k eod
+#PBS -e /scratch/st-ljfoster-1/ppicluster/scheduler/full_netw_analysis_MCPrevisions_large.err
+#PBS -o /scratch/st-ljfoster-1/ppicluster/scheduler/full_netw_analysis_MCPrevisions_large.out
 #PBS -m abe
 #PBS -M richard.greg.stacey@gmail.com
 #PBS -l mem=64gb   
-
-#SBATCH -o full_netw_analysis_MCPrevisions_large.out
-#SBATCH -e full_netw_analysis_MCPrevisions_large.err
-#SBATCH --mail-user=richard.greg.stacey@gmail.com
-#SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --time=71:55:00
-#SBATCH --mem=64G
-#SBATCH --array=1-216
-#SBATCH --account=rrg-ljfoster-ab
+#PBS -J 1-36
+#PBS -A st-ljfoster-1
 
 module load gcc/9.1.0
 module load python/3.7.3
 module load r/3.6.2-py3.7.3
 source ~/environments/ppicluster_venv/bin/activate
 
-Rscript full_netw_analysis_MCPrevisions.R $SLURM_ARRAY_TASK_ID large
+Rscript full_netw_analysis_MCPrevisions.R $PBS_ARRAYID large
