bff903312aab1ea6cd2307c9a6815458d0f887c2
Greg Stacey <gregstacey@Gregs-MacBook-Pro.local>
2020-07-16 21:35:29 -0700

    Remove arrays again

--- scheduler/full_netw_analysis_MCPrevisions.sh
+++ scheduler/full_netw_analysis_MCPrevisions.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 #PBS -N full_netw_analysis_MCPrevisions_large
 #PBS -l walltime=71:55:00
-#PBS -e /scratch/st-ljfoster-1/ppicluster/scheduler/full_netw_analysis_MCPrevisions_large.err
-#PBS -o /scratch/st-ljfoster-1/ppicluster/scheduler/full_netw_analysis_MCPrevisions_large.out
+#PBS -e /scratch/st-ljfoster-1/ppicluster/scheduler/5full_netw_analysis_MCPrevisions_large.err
+#PBS -o /scratch/st-ljfoster-1/ppicluster/scheduler/5full_netw_analysis_MCPrevisions_large.out
 #PBS -m abe
 #PBS -M richard.greg.stacey@gmail.com
 #PBS -l mem=64gb   
-#PBS -J 1-36
 #PBS -A st-ljfoster-1
+
+
+# # # # #PBS -J 1-36
+
 
 module load gcc/9.1.0
 module load python/3.7.3
 module load r/3.6.2-py3.7.3
 source ~/environments/ppicluster_venv/bin/activate
 
-echo 'the array number is'
-echo $PBS_ARRAY_INDEX
+#echo 'the array number is'
+#echo $PBS_ARRAY_INDEX
 
-Rscript ~/projects/ppicluster/R/full_netw_analysis_MCPrevisions.R $PBS_ARRAYID large
+Rscript ~/projects/ppicluster/R/full_netw_analysis_MCPrevisions.R 5 large
